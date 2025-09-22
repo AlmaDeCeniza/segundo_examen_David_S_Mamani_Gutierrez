a59f3b673dd719ad6a8c18643b4c229b2c86751c
David <dmgarg97@gmail.com>
2025-09-21 20:13:40 -0400

    feat(ui): showcase template components on/ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,23 @@
 import { Routes } from '@angular/router';
 import { Layout } from './layout/layout';
 import { Home } from './home/home';
+import { Ui } from './ui/ui';
 
-export const routes: Routes = [{
-    
-      path:'',
-        component:Layout,
-        children:[
-            {//OJO
-                path:'',
-                component:Home,
-                title:'Inicio'
-            }
-          
-        ]
-    }
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        component: Home,
+        title: 'Inicio'
+      },
+      {
+        path: 'ui', 
+        component: Ui,
+        title: 'UI'
+      }
+    ]
+  }
 ];
